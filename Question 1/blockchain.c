a1b943569879b6f0ea42d2ea096cdb84c0ab3793
Pascal Onuoha <116768306+iPascal619@users.noreply.github.com>
2025-02-24 22:16:58 +0200

    Update blockchain.c

--- Question 1/blockchain.c
+++ Question 1/blockchain.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <openssl/evp.h>  // Include for EVP API
+#include <openssl/evp.h>  
 #include "blockchain.h"
 
 void calculateHash(JobBlock* block, char* output) {
